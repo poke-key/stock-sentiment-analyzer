ef145b26e443f71be0058cc8f5c596f4a9480983
Salads <dgentry.email@protonmail.com>
2025-02-24 01:30:25 -0800

    Update python requirements.txt, Add comments to main.py

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,10 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+# API Routes go here
 @app.get("/api/hello")
 async def root():
     return {"message": "Hello World"}
 
+# Do NOT put API routes after this line!
 app.mount("/", StaticFiles(directory="frontend/dist", html=True), name="frontend")
