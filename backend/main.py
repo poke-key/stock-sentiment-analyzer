6f75006e92be0bcd343fda6c64beb15b111e045f
Salads <dgentry.email@protonmail.com>
2025-03-09 04:18:12 -0700

    Added signup (new account) api path.
    - Requires username, password, and signup password. Just so we dont get randoms eating api limits
    - Added "SIGNUP_PASS" to .env.example

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from config import config
 from db import DatabaseClient
@@ -8,7 +8,7 @@ app = FastAPI()
 # print("Database URL: " + config.database_url)
 # print("Stock API Key: " + config.stock_api_key)
 
-# db = DatabaseClient()
+db = DatabaseClient()
 # user = db.GetUser(username="testname")
 # if user:
 #     print("user already exists")
@@ -21,10 +21,20 @@ app = FastAPI()
 #     print(user)
 
 
+
 # API Routes go here
 @app.get("/api/hello")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/api/signup")
+async def request_new_account(username: str, password: str, signup_pass: str):
+    if signup_pass != config.signup_pass:
+        raise HTTPException(status_code=401, detail="signup code invalid")
+    user = db.GetUser(username)
+    if user:
+        raise HTTPException(status_code=401, detail="user already exists with that name")
+    db.AddUser(username, password)
 
 # Do NOT put API routes after this line!
 app.mount("/", StaticFiles(directory="frontend/dist", html=True), name="frontend")
