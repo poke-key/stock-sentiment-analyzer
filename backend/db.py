b489f45e9133a787b3b115c65b080ec742376e90
Salads <dgentry.email@protonmail.com>
2025-03-09 03:31:26 -0700

    Added postgres handing for session updating, and GetSessionValid function

--- backend/db.py
+++ backend/db.py
@@ -2,12 +2,21 @@ from config import config
 from fastapi import Depends
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from dbmodels import User
+import uuid
+import time
 
 engine = create_engine(str(config.database_url))
 SQLModel.metadata.create_all(engine)
 
 # AddUser(username, password)
 # GetUser(username)
+
+SessionTTL = 10*60 # in seconds
+
+def GetSessionValid(session: uuid.UUID, lastTime: float):
+	cur_time = time.time()
+	return (cur_time - lastTime) <= SessionTTL
+
 
 class DatabaseClient:
 
@@ -22,3 +31,15 @@ class DatabaseClient:
 			statement = select(User).where(User.username == username)
 			user = session.exec(statement).first()
 			return user
+
+	# Simply updates lastTime, TTL for session is 10 minutes
+	def UpdateUserSession(self, username):
+		with Session(engine) as session:
+			user = self.GetUser(username)
+			if user:
+				user.lastTime = time.time()
+				session.add(user)
+				session.commit()
+				session.refresh(user)
+			# else:
+				# TODO(Darrell): Logging System?
