bfc43477754b2e7993cbe11b2014899e104d1cbd
Salads <dgentry.email@protonmail.com>
2025-03-09 04:44:42 -0700

    Add singup and login api routes
    - Right now session tokens are just tokens that we'll use for other api routes, this should get a thin layer of security from randoms eating api limits
    - There is NO time limit on session tokens right now. It's just a "authentication" step right now.

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from fastapi.staticfiles import StaticFiles
 from config import config
-from db import DatabaseClient
+from db import DatabaseClient, GetSessionValid
+import uuid
+import time
 
 app = FastAPI()
 
@@ -27,7 +29,7 @@ db = DatabaseClient()
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/api/signup")
+@app.post("/api/signup", status_code=status.HTTP_201_CREATED)
 async def request_new_account(username: str, password: str, signup_pass: str):
     if signup_pass != config.signup_pass:
         raise HTTPException(status_code=401, detail="signup code invalid")
@@ -35,6 +37,24 @@ async def request_new_account(username: str, password: str, signup_pass: str):
     if user:
         raise HTTPException(status_code=401, detail="user already exists with that name")
     db.AddUser(username, password)
+    return {"message" : "OK"}
+
+@app.get("/api/login", status_code=status.HTTP_200_OK)
+async def login(username: str, password: str):
+    with Session(engine) as session:
+        user = db.GetUser(username)
+        if not user:
+            raise HTTPException(status_code=401, detail="user doesn't exist")
+        if user.password != password:
+            raise HTTPException(status_code=401, detail="invalid login credentials")
+        # Generate session token for use now.
+        user.session = uuid.uuid4()
+        user.lastTime = time.time()
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return {"session" : user.session}
+
 
 # Do NOT put API routes after this line!
 app.mount("/", StaticFiles(directory="frontend/dist", html=True), name="frontend")
