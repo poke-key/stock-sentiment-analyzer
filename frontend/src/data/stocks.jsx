b9a62b0de18c8bf46b4395db7d13c6c7bc91b2e9
Salads <dgentry.email@protonmail.com>
2025-03-13 00:39:06 -0700

    Update dropdown selection

--- frontend/src/data/stocks.jsx
+++ frontend/src/data/stocks.jsx
@@ -1,21 +1,7 @@
 export const techStockOptions = [
-	{ value: "NVDA", label: "Nvidia" },
-	{ value: "AMD", label: "AMD" },
-	{ value: "AVGO", label: "Broadcom" },
-	{ value: "INTC", label: "Intel" },
-	{ value: "MSFT", label: "Microsoft" },
-	{ value: "AMZN", label: "Amazon" },
-	{ value: "GOOGL", label: "Alphabet" },
-	{ value: "ORCL", label: "Oracle" },
-	{ value: "TSLA", label: "Tesla" },
-	{ value: "META", label: "Meta" },
-	{ value: "PLTR", label: "Palantir" },
-	{ value: "AI", label: "C3.ai" },
-	{ value: "CRWD", label: "CrowdStrike" },
-	{ value: "PANW", label: "Palo Alto Networks" },
-	{ value: "SNOW", label: "Snowflake" },
-	{ value: "GTLB", label: "GitLab" },
-	{ value: "DDOG", label: "Datadog" },
-	{ value: "MDB", label: "MongoDB" }
+	{label: "Semiconductors (NVDA, INTC)", value : ["NVDA", "INTC"]},
+	{label: "Cloud Services (MSFT, AMZN, GOOGL)", value : ["MSFT", "AMZN", "GOOGL"]},
+	{label: "AI Software (TSLA, META)", value : ["TSLA", "META"]},
+	{label: "Cybersecurity (CRWD, PANW)", value : ["NVDA", "INTC"]},
   ];
     
